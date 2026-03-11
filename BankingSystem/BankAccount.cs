b485a82730cfb172620a10f20df7f65f674842aa
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-11 16:05:16 +0000

    feat: Creating repo for BankSystem

---
+++ BankingSystem/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    internal class BankAccount
+    {
+        public int AccountNumber { get; }
+        public string OwnerName { get; }
+        public int Balance { get; }
+
+        public BankAccount(int accountNumber, string ownerName, int balance)
+        {
+            AccountNumber = accountNumber;
+            OwnerName = ownerName;
+            Balance = balance;
+        }
+
+    }
+}
---
+++ BankingSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BankingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount newAccount = new BankAccount(65431, "Jodie McLaughlin", 5000);
+
+            Console.WriteLine("Account number: " + newAccount.AccountNumber);
+            Console.WriteLine("Account owner: " + newAccount.OwnerName);
+            Console.WriteLine("Account balance: " + newAccount.Balance);
+
+        }
+    }
+}
